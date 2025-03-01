209945051bcd8f4fa79c765da8b00a9aadd18efc
holim chol <ghfla225@gmail.com>
2025-03-02 05:56:25 +0900

    9탄

--- NewYorkTimes/main.js
+++ NewYorkTimes/main.js
@@ -1,9 +1,12 @@
- const API_KEY = `73014994bd1943d29b4deae2f5e5a166`;
-let news =[]
+const API_KEY = `73014994bd1943d29b4deae2f5e5a166`;
+let newsList =[];
+const menus = document.querySelectorAll(".menus button");
+console.log("mmm",menus);
+menus.forEach(menus=>menus.addEventListener("click",(event)=>getNewsByCategory(event)));
+
 const getLatestNews = async() => {
   const url = new URL(
-    // `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&pageSize=10`
+    `https://newsapi.org/v2/top-headlines/sources?apiKey=${API_KEY}`
 );
 const response = await fetch(url);
 const data = await response.json();
@@ -12,9 +15,21 @@ render();
 console.log("news",newsList);
 };
 
+const getNewsByCategory = async (event) => {
+    const category = event.target.textContent.toLowerCase();
+    console.log("category",category);
+    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`);
+
+    const response =  await fetch(url);
+    const data = await response.json();
+    console.log("Ddd",data);
+    newsList = data.articles;
+    render();
+};
+
 const render = () => {
     const newsHTML = newsList.map(
-        news => ` <div class="row news">  
+        (news) => ` <div class="row news">  
             <div class="col-lg-4">
                 <img class="news-img-size" 
                 src= ${news.urlToImage} />
@@ -27,12 +42,15 @@ const render = () => {
                 <div>
                     ${news.source.name} * ${news.publishedAt}
                 </div>
-            </div>`
+            </div>
+        </div>`
         ).join(""); 
-        console.log("html",newsHTML);
     
     document.getElementById("news-board").innerHTML = newsHTML;
 };
 
-
 getLatestNews();
+
+//1. 버튼들에 클릭 이벤트주기
+//2. 카테고리별 뉴스 가져오기
+//3. 그 뉴스를 보여주기
